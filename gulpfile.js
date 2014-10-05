18aaf08fd53859e944d2b9b45a4aee61c7ee6914
daikissdd <daikissdd@gmail.com>
2014-10-05 17:16:54 +0700

    update

--- gulpfile.js
+++ gulpfile.js
@@ -3,69 +3,47 @@ var gulp = require('gulp'),
 	uglify = require('gulp-uglify'),
 	minifyCss = require('gulp-minify-css'),
 	imagemin = require('gulp-imagemin');
+	config = require('./gulpconfig.js');
 
 var src = {img: 'assets/img', js: 'assets/js', css: 'assets/css'},
-	dist = {img: 'assets/dist/img', js: 'assets/dist/js', css: 'assets/dist/css'},
-	bower = 'assets/bower_components/',
-	js = 'assets/js/',
-	ctrl = js + 'controllers/',
-	conf = js + 'config/',
-	service = js + 'services/',
-	directive = js + 'directives/',
-	distjs = 'assets/dist/js/all.js';
+	dist = {img: 'assets/dist/assets/img', js: 'assets/dist/assets/js', css: 'assets/dist/assets/css'},
+	jsfiles = config.get();
 
-var jsfiles = [
-    bower + 'lodash/dist/lodash.min.js',
-    bower + 'jquery/dist/jquery.min.js',
-    bower + 'aws-sdk-js/dist/aws-sdk.min.js',
-	bower + 'angular/angular.min.js',
-	bower + 'sass-bootstrap/dist/js/bootstrap.min.js',
-	bower + 'jquery.transit/jquery.transit.js',
-	bower + 'angular-animate/angular-animate.min.js',
-	bower + 'angular-route/angular-route.min.js',
-	bower + 'angular-cookies/angular-cookies.min.js',
-	bower + 'angular-touch/angular-touch.min.js',
-	bower + 'angular-ui-bootstrap-bower/ui-bootstrap-tpls.min.js',
-	bower + 'angular-bootstrap-validation/dist/angular-bootstrap-validation.js',
-	bower + 'angular-bootstrap-notifications/notifications.min.js',
-	bower + 'angular-busy/dist/angular-busy.min.js',
-	bower + 'angularLocalStorage/src/angularLocalStorage.js',
-	js + 'app.js',
-	directive + 'copyright.js',
-	directive + 'modalside.js',
-	service + 'url.js',
-	service + 'geturl.js',
-	service + 'product.js',
-	service + 'wall.js',
-	service + 'card.js',
-	service + 'mail.js',
-	service + 'modal.js',
-	service + 'switcher.js',
-	service + 'latest.js',
-	ctrl + 'app-ctrl.js',
-	ctrl + 'password-ctrl.js',
-	ctrl + 'modal-ctrl.js',
-	ctrl + 'home-ctrl.js',
-	ctrl + 'wall-ctrl.js',
-	ctrl + 'admin-ctrl.js',
-	ctrl + 'card-ctrl.js'
-];
-
-gulp.task('concat', function() {
+gulp.task('js', function() {
 	gulp.src(jsfiles)
 	.pipe(concat('all.js'))
 	.pipe(gulp.dest(dist.js));
 });
 
+gulp.task('css', function() {
+	gulp.src([
+		src.css + '/bootstrap.css',
+		src.css + '/main.css',
+	])
+	.pipe(concat('all.css'))
+	.pipe(minifyCss())
+	.pipe(gulp.dest(dist.css));
+});
+
+gulp.task('image', function () {
+  return gulp.src(src.img + '/**/*.{png,jpg,gif}')
+    .pipe(imagemin({
+		progressive: true,
+		interlaced: true
+    }))
+    .pipe(gulp.dest(dist.img));
+});
+
 gulp.task('copy', function() {
-	//gulp.src(src.img + '/**').pipe(gulp.dest(dist.img));
-	//gulp.src(src.css + '/**').pipe(gulp.dest(dist.css));
 	var copy = ['assets/bower_components/angular-touch/angular-touch.min.js.map'];
 	gulp.src(copy).pipe(gulp.dest(dist.js));
 });
 
 gulp.task('default', function() {
-	console.log('-- daikissdd -- gulp start!!');
-	gulp.run('concat');
+	var white = '\u001b[37m', reset = '\u001b[0m', n = '\n';
+	console.log(n + white + '*** Gulp task start. My script working in vietnam!! ***' + reset + n);
+	gulp.run('js');
+	gulp.run('css');
 	gulp.run('copy');
+	gulp.run('image');
 });
